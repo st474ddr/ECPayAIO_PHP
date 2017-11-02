8dfa89b8c316ef105b9f016357293d4095de20d9
allen.chang <allen.chang@allpay.com.tw>
2017-11-02 16:18:12 +0800

    修正 SDK 無法使用參數ClientReplyURL的問題

--- PayLogisticSDK/sample/sample_CvsCreateShippingOrder.php
+++ PayLogisticSDK/sample/sample_CvsCreateShippingOrder.php
@@ -1,5 +1,5 @@
 ﻿<?php
-    // 宅配逆物流訂單建立
+    // 一般物流訂單建立
     define('HOME_URL', 'http://www.sample.com.tw/logistics_dev');
     require('ECPay.Logistics.Integration.php');
     try {
@@ -8,26 +8,35 @@
         $AL->HashIV = 'v77hoKGq4kWxNNIS';
         $AL->Send = array(
             'MerchantID' => '2000132',
-            'AllPayLogisticsID' => '15609',
-			'LogisticsType' => LogisticsType::HOME,
-            'LogisticsSubType' => LogisticsSubType::TCAT,
+            'MerchantTradeNo' => 'no' . date('YmdHis'),
+            'MerchantTradeDate' => date('Y/m/d H:i:s'),
+			'LogisticsType' => LogisticsType::CVS,
+            'LogisticsSubType' => LogisticsSubType::UNIMART,
+            'GoodsAmount' => 1500,
+            'CollectionAmount' => 10,
+            'IsCollection' => IsCollection::NO,
+            'GoodsName' => '測試商品',
             'SenderName' => '測試寄件者',
             'SenderPhone' => '0226550115',
-            'SenderCellPhone' => '0933222111',
-            'SenderZipCode' => '11560',
-            'SenderAddress' => '台北市南港區三重路19-2號5樓D棟',
+            'SenderCellPhone' => '0911222333',
             'ReceiverName' => '測試收件者',
-            'ReceiverPhone' => '0226550116',
-            'ReceiverCellPhone' => '0911222333',
+            'ReceiverPhone' => '0226550115',
+            'ReceiverCellPhone' => '0933222111',
             'ReceiverEmail' => 'test_emjhdAJr@test.com.tw',
-            'ReceiverZipCode' => '11560',
-            'ReceiverAddress' => '台北市南港區三重路19-2號5樓D棟',
+            'TradeDesc' => '測試交易敘述',
             'ServerReplyURL' => HOME_URL . '/ServerReplyURL.php',
-			'GoodsAmount' => 1,
+            'ClientReplyURL' => HOME_URL . '/ClientReplyURL.php',
+            'LogisticsC2CReplyURL' => HOME_URL . '/LogisticsC2CReplyURL.php',
+            'Remark' => '測試備註',
             'PlatformID' => '',
         );
-        // CreateHomeReturnOrder()
-        $Result = $AL->CreateHomeReturnOrder();
+
+        $AL->SendExtend = array(
+            'ReceiverStoreID' => '991182',
+            'ReturnStoreID' => '',
+        );
+        // CreateShippingOrder()
+        $Result = $AL->CreateShippingOrder();
         echo '<pre>' . print_r($Result, true) . '</pre>';
     } catch(Exception $e) {
         echo $e->getMessage();
