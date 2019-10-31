020054472c25b5119552644ceda2f083aa85ccf8
adela.chang <adela.chang@ecpay.com.tw>
2019-10-31 13:58:28 +0800

    修正開立發票如有帶()會出現檢查碼錯誤的問題

---
+++ AioSDK/example/sample_FundingReconDetail.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*   一般產生訂單(全功能)範例
+*/
+    
+    //載入SDK(路徑可依系統規劃自行調整)
+    include('ECPay.Payment.Integration.php');
+    try {
+        
+    	$obj = new ECPay_AllInOne();
+   
+        //服務參數
+        $obj->ServiceURL  = "https://payment.ecpay.com.tw/CreditDetail/FundingReconDetail";  //服務位置
+        $obj->HashKey     = 'yjdty2S8vaqc8gGv' ;                                          //測試用Hashkey，請自行帶入ECPay提供的HashKey
+        $obj->HashIV      = 'jnMehSf8lyrVaegG' ;                                          //測試用HashIV，請自行帶入ECPay提供的HashIV
+        $obj->MerchantID  = '1099236';                                                    //測試用MerchantID，請自行帶入ECPay提供的MerchantID
+        $obj->EncryptType = '1';                                                          //CheckMacValue加密類型，請固定填入1，使用SHA256加密
+
+
+        //基本參數(請依系統規劃自行調整)
+
+        $obj->Funding['PayDateType']   = 'close';                         
+        $obj->Funding['StartDate']     = '2017-09-01';                      
+        $obj->Funding['EndDate']       = '2017-12-31';                      
+
+        $result = $obj->FundingReconDetail();
+        echo $result ;
+        file_put_contents('new.csv', $result);
+
+    } catch (Exception $e) {
+    	echo $e->getMessage();
+    } 
+
+?>
